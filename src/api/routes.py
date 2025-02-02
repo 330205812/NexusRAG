3226af61984a9907c1cac73213f7bc3b088606d2
zhangwq5 <zhangwq5@sugon.com>
2025-02-02 21:19:58 +0800

    update config & docker compose file

---
+++ src/api/routes.py
@@ -0,0 +1,6 @@
+from fastapi import FastAPI
+from ..api.endpoints.knowledge_base_endpoints import router
+
+
+def setup_routes(app: FastAPI):
+    app.include_router(router, tags=["knowledge_base"])
